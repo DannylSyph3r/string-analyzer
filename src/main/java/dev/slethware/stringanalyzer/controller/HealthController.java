73b2161cd9ed6aee068ea06ca7bd90f25d659ac2
DannylSyph3r <dopedanny001@gmail.com>
2025-10-21 11:05:12 +0100

    Health check to base url

---
+++ src/main/java/dev/slethware/stringanalyzer/controller/HealthController.java
@@ -0,0 +1,25 @@
+package dev.slethware.stringanalyzer.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+@Hidden
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+        response.put("message", "String Analyzer API is running");
+        response.put("version", "1.0");
+        return ResponseEntity.ok(response);
+    }
+}
